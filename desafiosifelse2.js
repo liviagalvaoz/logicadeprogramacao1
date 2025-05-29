44900a7fe75b7e4a4eb17cf1b193795f5adbb5c1
Lívia Galvão <liviagalvao2009@gmail.com>
2025-05-29 11:34:48 -0300

    desafiosifelse2.js

--- desafiosifelse2.js
+++ desafiosifelse2.js
@@ -3,25 +3,25 @@ LOGPRO IF ELSE 2
 Teste
 //teste
 
-/*let nome = prompt(&quot;Digite seu nome: &quot;)
-let idade = Number(prompt(&quot;Digite a sua idade: &quot;))
-console.log(&quot;O seu nome&quot;, nome, &quot;você tem&quot;, idade, &quot;anos&quot;)*/
+let nome = prompt("Digite seu nome:")
+let idade = Number(prompt("Digite a sua idade:"))
+console.log("O seu nome", nome, "você tem", idade, "anos")
 
 Cadastro do site
 //Cadastro do site
 
-/*let nome = prompt(&quot;Insira seu nome: &quot;)
-let email = (prompt(&quot;Insira seu email: &quot;))
-let telefone = Number(prompt(&quot;Insira seu telefone: &quot;))
-console.log(&quot;A conta foi criada no nome de&quot;, nome, &quot;,o endereco
-de email é&quot;, email, &quot;e o numero de telefone para contato é &quot;,
-telefone)*/
+let nome = prompt("Insira seu nome: ")
+let email = (prompt("Insira seu email: "))
+let telefone = Number(prompt("Insira seu telefone: "))
+console.log("A conta foi criada no nome de", nome, ",o endereco
+de email é", email,"e o numero de telefone para contato é",
+telefone)
 
 Descobrindo a idade
 //Descobrindo a idade
 
-/*let Nascimento = Number(prompt(&quot;Digite sua ano de
-nascimento: &quot;))
+let Nascimento = Number(prompt("Digite sua ano de
+nascimento: "))
 2025-Nascimento
-console.log(2025-Nascimento)*/
+console.log(2025-Nascimento)
 
