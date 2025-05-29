e0e79049fda2228d54783f737ff3bd51cbfd2399
livia <liviagalvao2009@gmail.com>
2025-05-29 13:46:29 -0300

    funcao2

---
+++ funcao2.js
@@ -0,0 +1,50 @@
+FUNÇAO 2
+
+function obterusuario() {
+  return prompt("Digite o usuário:");
+}
+
+function obtersenha() {
+  return prompt("Digite a senha:");
+}
+
+function verificarlogin(usuario, senha) {
+  let usuario1 = "sccp";
+  let senha1 = "1910";
+
+  let usuario2 = "coringaogigante";
+  let senha2 = "2000";
+
+  let usuario3 = "corinthians";
+  let senha3 = "2012";
+
+  let usuario4 = "campeao";
+  let senha4 = "2012";
+
+  let usuario5 = "timao";
+  let senha5 = "1990";
+
+  let usuario6 = "fiel";
+  let senha6 = "1969";
+
+  let usuario7 = "maiordesp";
+  let senha7 = "2009";
+
+  let usuario8 = "campeao";
+  let senha8 = "1999";
+
+  let usuario9 = "todopoderoso";
+  let senha9 = "1961";
+
+  let usuario10 = "coringao";
+  let senha10 = "2015";
+
+  if (usuario === usuario1 && senha === senha1) {
+    console.log("Login concluído.");
+  } else if (usuario === usuario2 && senha === senha2) {
+    console.log("Login concluído.");
+  } else if (usuario === usuario3 && senha === senha3) {
+    console.log("Login concluído.");
+  } else if (usuario === usuario4 && senha === senha4) {
+    console.log("Login concluído.");
+  } else if (usuario === usuario5 && senha ==
