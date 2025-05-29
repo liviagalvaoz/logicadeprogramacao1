897eb9df617e3f307eb94e1522da4e0785362031
livia <liviagalvao2009@gmail.com>
2025-05-29 11:26:55 -0300

    ifelse2

---
+++ desafiosifelse2.js
@@ -0,0 +1,27 @@
+LOGPRO IF ELSE 2
+
+Teste
+//teste
+
+/*let nome = prompt(&quot;Digite seu nome: &quot;)
+let idade = Number(prompt(&quot;Digite a sua idade: &quot;))
+console.log(&quot;O seu nome&quot;, nome, &quot;você tem&quot;, idade, &quot;anos&quot;)*/
+
+Cadastro do site
+//Cadastro do site
+
+/*let nome = prompt(&quot;Insira seu nome: &quot;)
+let email = (prompt(&quot;Insira seu email: &quot;))
+let telefone = Number(prompt(&quot;Insira seu telefone: &quot;))
+console.log(&quot;A conta foi criada no nome de&quot;, nome, &quot;,o endereco
+de email é&quot;, email, &quot;e o numero de telefone para contato é &quot;,
+telefone)*/
+
+Descobrindo a idade
+//Descobrindo a idade
+
+/*let Nascimento = Number(prompt(&quot;Digite sua ano de
+nascimento: &quot;))
+2025-Nascimento
+console.log(2025-Nascimento)*/
+
