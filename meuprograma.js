dab8d96fb7d46204b32cb6e573059962a8469180
livia <liviagalvao2009@gmail.com>
2025-05-29 11:01:49 -0300

    meuprograma

---
+++ meuprograma.js
@@ -0,0 +1,51 @@
+Teste
+//teste
+// 1. Solicitando nome e idade via prompt e exibindo no console
+let nome = prompt("Digite seu nome: ")
+let idade = Number(prompt("Digite a sua idade: "))
+console.log("O seu nome", nome, "você tem", idade, "anos")
+
+// 2. Cadastro do site
+let nomeCadastro = prompt("Insira seu nome: ")
+let email = prompt("Insira seu email: ")
+let telefone = Number(prompt("Insira seu telefone: "))
+console.log(
+  "A conta foi criada no nome de", nomeCadastro, 
+  ", o endereço de email é", email, 
+  "e o número de telefone para contato é", telefone
+)
+
+// 3. Descobrindo a idade
+let nascimento = Number(prompt("Digite seu ano de nascimento: "))
+let idadeAtual = 2025 - nascimento
+console.log("Sua idade é", idadeAtual)
+
+
+
+
+LOGPRO  Seu_Programa JS 
+
+
+let inicio = "Seja bem-vindo ao site do Fiel Torcedor"
+console.log(inicio)
+
+let cadastro = "Cadastre-se ou faça login"
+console.log(cadastro)
+
+let nome = prompt("Insira seu nome: ")
+let email = prompt("Insira seu email: ")
+let senha = prompt("Insira sua senha: ")
+let telefone = Number(prompt("Insira seu telefone para mais opções de contato (opcional): "))
+console.log("A conta foi criada no nome de", nome, ",o endereco de email é", email, "e o numero de telefone para contato é ", telefone)
+
+let pesquisa = prompt("Pesquise por ingressos, loja, mais opções de plano: ")
+console.log("Aba de pesquisa", pesquisa)
+
+let ingresso = prompt("Ingressos para o jogo Corinthians x Santos disponíveis nos setores: Leste e Sul: ")
+console.log("Comprar", ingresso)
+
+let formadepgt = prompt("Informe o método de pagamento:")
+console.log(formadepgt)
+
+
+
