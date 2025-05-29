eb413ba981f929ee33fd6ab3b83dc647fb5dabcf
livia <liviagalvao2009@gmail.com>
2025-05-29 10:57:47 -0300

    variaveis1

---
+++ variaveis1.js
@@ -0,0 +1,67 @@
+
+LOGPRO Atividade 1 - Variaveis 
+
+1. Declarando uma variável e exibindo no console
+//meu primeiro código 1
+let nome = "Lívia"
+console.log(nome)
+
+2. Declarando um número e exibindo no console
+//idade 2
+let idade = 15
+console.log(idade)
+
+3. Criando uma variável booleana
+// estou aprendendo javascript? 3
+let aprendendojavascript = true
+console.log(aprendendojavascript)
+
+4. Concatenando strings
+//nome completo 4
+let primeironome = "Lívia "
+let segundonome = "Nunes "
+let terceironome = "Galvão "
+console.log(primeironome + segundonome + terceironome)
+
+5. Somando dois números
+//soma 5
+let numero1 = 15
+let numero2 = 15
+let soma = numero1 + numero2
+console.log(soma)
+
+6. Multiplicando dois números
+// multiplicação 6
+let numero3 = 10
+let numero4 = 20
+let multiplicacao = numero3 * numero4
+console.log(multiplicacao)
+
+7. Variável de texto e número
+// cidade 7
+let cidade = "Ferraz de Vasconcelos"
+console.log(cidade)
+let habitantes = 179198
+console.log(habitantes)
+
+8. Alterando o valor de uma variável
+// preco 8
+let preco = 20
+preco += 30
+console.log(preco)
+
+9. Criando uma variável sem valor (undefined)
+//undefined 9
+let meuvalor
+console.log(meuvalor)
+
+10. Criando um valor null
+//null 10
+let meunulo = null
+console.log(meunulo)
+
+
+
+
+
+
